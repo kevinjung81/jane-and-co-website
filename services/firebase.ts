3c70963e506194923876dd88a7c7c6e0ac70be26
Kevin Jung <kevinjung81@gmail.com>
2026-02-15 10:34:14 -0600

    Secure API key and update gitignore

--- services/firebase.ts
+++ services/firebase.ts
@@ -6,14 +6,16 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // NOTE: Replace these with your actual Firebase project configuration keys.
 const firebaseConfig = {
-  apiKey: "AIzaSyDwXK3juvSQ07uRyHbyBfx-8wFfnujxTNM",
   authDomain: "jane-and-company-website.firebaseapp.com",
+  apiKey: import.meta.env.VITE_GEMINI_API_KEY,
   projectId: "jane-and-company-website",
   storageBucket: "jane-and-company-website.firebasestorage.app",
   messagingSenderId: "1050356796526",
   appId: "1:1050356796526:web:bfc98bf09b51df6e68aa7f",
   measurementId: "G-21539M55WQ"
 };
+
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 
 // Initialize Firebase
 // We use a check to avoid re-initialization in strict mode or hot reloads
